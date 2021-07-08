2119983049b6badf0670f23c5e71fe2f69b022df
Lena Eivy <applena@gmail.com>
2021-07-08 15:23:30 -0700

    add event working

--- src/components/addEvent.js
+++ src/components/addEvent.js
@@ -1,13 +1,13 @@
 /* global gapi */
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import "react-datepicker/dist/react-datepicker.css";
 import { connect } from 'react-redux';
-import {setCalendars, toggleHideCalendar, setConfig, setMyQCalendar} from '../store/actions';
+import { setCalendars, toggleHideCalendar, setConfig, setMyQCalendar } from '../store/actions';
 
 import DateTimePicker from 'react-datetime-picker';
 
@@ -15,55 +15,57 @@ function AddEvent(props) {
   const [attendeeEmail, setAttendeeEmail] = useState([])
   const [show, setShow] = useState(false);
   const [eventName, setEventName] = useState('');
-  const [date, setDate] = useState(new Date());
+  const [date] = useState(new Date());
   const [location, setEventLocation] = useState('');
   const [description, setDescription] = useState('');
   const [value, setValue] = useState(new Date());
+  const [displayRecurringEvent, setDisplayRecurringEvent] = useState(false);
+  const [recurringValue, setRecurringValue] = useState(false);
 
   const handleClose = () => setShow(false);
 
   const saveEvent = () => {
 
-    console.log({value}, value.toISOString(), date);
+    console.log({ value }, 'ISO:', value.toISOString(), 'date:', date);
     const event = {
       'summary': eventName,
       'location': location,
       'description': description,
       'start': {
-        'dateTime': value.toISOString(),//'2015-05-28T09:00:00-07:00',
+        'dateTime': value.toISOString(),//'2015-05-28T09:00:00-07:00', 2021-07-10T21:20:24.453Z
         'timeZone': date.toString().match(/[A-Z]{3}/)[0]
       },
       'end': {
         'dateTime': value.toISOString(),//'2015-05-28T17:00:00-07:00', TODO - add an hour
         'timeZone': date.toString().match(/[A-Z]{3}/)[0]
       },
-      'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
-      ],
+      // 'recurrence': [
+      //   'RRULE:FREQ=DAILY;COUNT=2'
+      // ],
       'attendees': attendeeEmail,
-      'reminders': {
-        'useDefault': false,
-        'overrides': [
-          {'method': 'email', 'minutes': 24 * 60},
-          {'method': 'popup', 'minutes': 10}
-        ]
-      }
+      // 'reminders': {
+      //   'useDefault': false,
+      //   'overrides': [
+      //     { 'method': 'email', 'minutes': 24 * 60 },
+      //     { 'method': 'popup', 'minutes': 10 }
+      //   ]
+      // }
     };
 
-    console.log({event})
-    
+    console.log({ event })
+
     const request = gapi.client.calendar.events.insert({
       'calendarId': props.myQCalendar.id,
       'resource': event
     });
-    
-    request.execute(function(event) {
+
+    request.execute(function (event) {
       console.log('Event created: ' + event.htmlLink);
     });
   }
 
 
-  return(
+  return (
     <>
       <button onClick={() => setShow(true)}>Add Event To MyQ</button>
 
@@ -120,11 +122,29 @@ function AddEvent(props) {
               </Form.Group>
               <Form.Group controlId="formEmailInvite">
                 <label>Email of Attendee</label>
-                <Form.Control onChange={(e) => setAttendeeEmail([{email: e.target.value}])} type="text" placeholder="Enter Attendee Email" />
+                <Form.Control onChange={(e) => setAttendeeEmail([{ email: e.target.value }])} type="text" placeholder="Enter Attendee Email" />
               </Form.Group>
               <Form.Group controlId="formRecurringEvent">
-                <Form.Check type="checkbox" label="Recurring Event" />
+                <Form.Check onClick={setDisplayRecurringEvent(!displayRecurringEvent)} type="checkbox" label="Recurring Event" />
               </Form.Group>
+
+              {displayRecurringEvent &&
+                <Form.Group controlId="formRecurringEventDetails">
+                  <Form.Control as="select">
+                    <option>Daily</option>
+                    <option>Weekly</option>
+                    <option>Monthly</option>
+                    <option>Yearly</option>
+                  </Form.Control>
+
+                  <Form.label>Ends On</Form.label>
+                  <DateTimePicker
+                    onChange={setRecurringValue}
+                    value={recurringValue}
+                    disableClock={true}
+                  />
+                </Form.Group>
+              }
             </Form>
           </Modal.Body>
 
@@ -138,14 +158,15 @@ function AddEvent(props) {
   )
 }
 
-const mapDispatchToProps = {setCalendars, toggleHideCalendar, setConfig, setMyQCalendar};
+const mapDispatchToProps = { setCalendars, toggleHideCalendar, setConfig, setMyQCalendar };
 
 const mapStateToProps = state => {
   console.log('calendar: mapStateToProps:', state);
   return ({
-  calendars: state.reduxData.calendars,
-  config: state.reduxData.config,
-  myQCalendar: state.reduxData.myQCalendar})
+    calendars: state.reduxData.calendars,
+    config: state.reduxData.config,
+    myQCalendar: state.reduxData.myQCalendar
+  })
 };
 
 export default connect(
--- src/components/upcomingEvents.js
+++ src/components/upcomingEvents.js
@@ -4,16 +4,17 @@ import React from 'react';
 import './upcomingEvents.scss';
 
 function UpcomingEvents(events) {
-  console.log('events', {events})
-  return(
+  console.log('events', { events })
+  return (
     <>
       <ul id="upcoming-events">
         {events.events.map((event, idx) => (
-          <li style={{borderLeft: `10px solid ${event.color}`, listStyle: 'none'}} key={idx}>
+          <li style={{ borderLeft: `10px solid ${event.color}`, listStyle: 'none' }} key={idx}>
             <div>Calendar: {event.calendar}</div>
             <div>Name: {event.event}</div>
-            <div>Start Time: {event.startTime}</div>
-            <div>End Time: {event.endTime}</div>
+            <div>Date: {new Date(event.startTime).toLocaleDateString()}</div>
+            <div>Start Time: {new Date(event.startTime).toLocaleString([], { hour: '2-digit', minute: '2-digit' })}</div>
+            <div>End Time: {new Date(event.endTime).toLocaleString([], { hour: '2-digit', minute: '2-digit' })}</div>
           </li>
         ))}
       </ul>
