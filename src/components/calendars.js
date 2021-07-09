dd48c1d78d90e6e4e3b1d58161783d440eaeca02
Lena Eivy <applena@gmail.com>
2021-07-09 10:38:52 -0700

    added console.log to calendars

--- src/components/calendars.js
+++ src/components/calendars.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { setCalendars, toggleHideCalendar, setConfig, setMyQCalendar } from '../store/actions';
 
 function Calendars(props) {
+  console.log('in Calendars', props)
 
   return (
     <div>
