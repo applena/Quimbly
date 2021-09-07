ad4567e9fff84199b45d8731d5913949e3a7e4ff
Lena Eivy <applena@gmail.com>
2021-09-07 14:18:39 -0700

    flushing out the add event form

--- src/components/addEvent.js
+++ src/components/addEvent.js
@@ -7,6 +7,7 @@ import React, { useState } from 'react';
 import "react-datepicker/dist/react-datepicker.css";
 import { connect } from 'react-redux';
 import { setCalendars, toggleHideCalendar, setConfig, setMyQCalendar } from '../store/actions';
+import moment from 'moment';
 
 import Modal from "@material-ui/core/Modal";
 import Card from '@material-ui/core/Card';
@@ -14,37 +15,53 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+import './addEvent.scss';
 
 
 function AddEvent(props) {
-  console.log('addEvent', { props });
+  //console.log('addEvent', { props });
+
+  const today = new Date().toISOString().split('T')[0];
+  const now = new Date().toISOString().split('T')[1].substring(0, 5);
+  const oneHour = new Date(new Date().setHours(new Date().getHours() + 1)).toISOString().split('T')[1].substring(0, 5);
+
   const [attendeeEmail, setAttendeeEmail] = useState([])
   const [show, setShow] = useState(false);
   const [eventName, setEventName] = useState('');
-  const [date] = useState(new Date());
   const [location, setEventLocation] = useState('');
   const [description, setDescription] = useState('');
-  const [value, setValue] = useState(new Date());
+  const [eventDate, setEventDate] = useState(today);
+  const [eventStartTime, setEventStartTime] = useState(now);
+  const [eventEndTime, setEventEndTime] = useState(oneHour);
   const [displayRecurringEvent, setDisplayRecurringEvent] = useState(false);
+
   // const [recurringValue, setRecurringValue] = useState(false);
 
-  console.log({ eventName })
+  console.log({ attendeeEmail })
+
+  const startDateTime = moment(`${eventDate} ${eventStartTime}`, 'YYYY-MM-DD HH:mm').format();
+  const endDateTime = moment(`${eventDate} ${eventEndTime}`, 'YYYY-MM-DD HH:mm').format();
   const handleClose = () => setShow(false);
 
   const saveEvent = () => {
 
-    console.log({ value }, 'ISO:', value.toISOString(), 'date:', date);
     const event = {
       'summary': eventName,
       'location': location,
       'description': description,
       'start': {
-        'dateTime': value.toISOString(),//'2015-05-28T09:00:00-07:00', 2021-07-10T21:20:24.453Z
-        'timeZone': date.toString().match(/[A-Z]{3}/)[0]
+        'dateTime': startDateTime,//'2015-05-28T09:00:00-07:00', 2021-07-10T21:20:24.453Z
+        'timeZone': props.myQCalendar.timeZone// default Quimbly time zone
       },
       'end': {
-        'dateTime': value.toISOString(),//'2015-05-28T17:00:00-07:00', TODO - add an hour
-        'timeZone': date.toString().match(/[A-Z]{3}/)[0]
+        'dateTime': endDateTime,//'2015-05-28T17:00:00-07:00', TODO - add an hour
+        'timeZone': props.myQCalendar.timeZone
       },
       // 'recurrence': [
       //   'RRULE:FREQ=DAILY;COUNT=2'
@@ -69,20 +86,13 @@ function AddEvent(props) {
     request.execute(function (event) {
       console.log('Event created: ' + event.htmlLink);
     });
+
+    handleClose();
   }
 
-  const today = new Date().toISOString().split('T')[0];
-  const now = new Date().toISOString().split('T')[1].substring(0, 5);
-  const oneHour = new Date(new Date().setHours(new Date().getHours() + 1)).toISOString().split('T')[1].substring(0, 5);
-
-  // const year = new Date().getUTCFullYear();
-  // const month = new Date().getUTCMonth().length < 2 ? `'0'+${new Date().getUTCMonth()}` : new Date().getUTCMonth();
-  // const day = new Date().getUTCDay().length < 2 ? `'0'+${new Date().getUTCDay()}` : new Date().getUTCDay();
-  // const today = `${year}-${month}-${day}`;
-
   return (
-    <>
-      <Button variant="outlined" onClick={() => setShow(true)}>Add Event To MyQ</Button>
+    <div id="addEvent">
+      <Button variant="outlined" onClick={() => setShow(true)}>Add Event To Quimbly</Button>
 
       <Modal
         open={show}
@@ -96,92 +106,76 @@ function AddEvent(props) {
               <TextField onChange={(e) => setEventLocation(e.target.value)} id="event-location" label="Event Location" />
               <TextField onChange={(e) => setDescription(e.target.value)} id="event-description" label="Event Description" />
               <TextField
+                onChange={(e) => setEventDate(e.target.value)}
                 type="date"
                 defaultValue={today}
                 id="event-date"
                 label="Event Date" />
               <TextField
+                onChange={(e) => setEventStartTime(e.target.value)}
                 label="Event Start Time"
                 type="time"
                 defaultValue={now}
                 id="event-start-time" />
               <TextField
+                onChange={(e) => setEventEndTime(e.target.value)}
                 label="Event End Time"
                 type="time"
                 defaultValue={oneHour}
                 id="event-end-time" />
+              <TextField
+                onChange={(e) => setAttendeeEmail([...attendeeEmail, { email: e.target.value }])}
+                label="Email of Attendee"
+                id="attendee-email" />
+              <FormControl component="fieldset">
+                <FormGroup>
+                  <FormControlLabel
+                    control={<Checkbox onChange={() => setDisplayRecurringEvent(!displayRecurringEvent)} />}
+                    label="Recurring Event"
+                  />
+                </FormGroup>
+              </FormControl>
               <Button onClick={() => setShow(false)} variant="outlined" color="secondary">Close</Button>
               <Button onClick={saveEvent} variant="outlined" color="primary">Save changes</Button>
             </form>
           </CardContent>
         </Card>
       </Modal>
-      {/* <Modal show={show} onHide={handleClose} animation={true}>
-        <Modal.Dialog>
-          <Modal.Header closeButton>
-            <Modal.Title>Add Event</Modal.Title>
-          </Modal.Header>
+    </div>
 
-          <Modal.Body>
-            <Form>
-              <Form.Group controlId="formEventName">
-                <Form.Label>Event Name</Form.Label>
-                <Form.Control onChange={(e) => setEventName(e.target.value)} type="text" placeholder="Enter event" />
-              </Form.Group>
 
-              <Form.Group controlId="formEventName">
-                <Form.Label>Location</Form.Label>
-                <Form.Control onChange={(e) => setEventLocation(e.target.value)} type="text" placeholder="Location" />
-              </Form.Group>
 
-              <Form.Group controlId="formEventName">
-                <Form.Label>Description</Form.Label>
-                <Form.Control onChange={(e) => setDescription(e.target.value)} type="text" placeholder="Description" />
-              </Form.Group>
+    // <Form.Group controlId="formRecurringEvent">
+    //   <Form.Check onClick={() => setDisplayRecurringEvent(!displayRecurringEvent)} type="checkbox" label="Recurring Event" />
+    // </Form.Group>
 
-              <Form.Group controlId="formDate">
-                <label>Date</label>
-                <DateTimePicker
-                  onChange={setValue}
-                  value={value}
+    // {displayRecurringEvent &&
+    //   <Form.Group controlId="formRecurringEventDetails">
+    //     <Form.Control as="select">
+    //       <option>Daily</option>
+    //       <option>Weekly</option>
+    //       <option>Monthly</option>
+    //       <option>Yearly</option>
+    //     </Form.Control>
+
+    //     <Form.Label>Ends On</Form.Label> */}
+    /* <DateTimePicker
+                  onChange={setRecurringValue}
+                  value={recurringValue}
                   disableClock={true}
-                />
-              </Form.Group>
-              <Form.Group controlId="formEmailInvite">
-                <label>Email of Attendee</label>
-                <Form.Control onChange={(e) => setAttendeeEmail([{ email: e.target.value }])} type="text" placeholder="Enter Attendee Email" />
-              </Form.Group>
-              <Form.Group controlId="formRecurringEvent">
-                <Form.Check onClick={() => setDisplayRecurringEvent(!displayRecurringEvent)} type="checkbox" label="Recurring Event" />
-              </Form.Group>
+  //               /> */
+    // </Form.Group>
+    //           }
+    //         </Form>
+    //       </Modal.Body>
 
-              {displayRecurringEvent &&
-                <Form.Group controlId="formRecurringEventDetails">
-                  <Form.Control as="select">
-                    <option>Daily</option>
-                    <option>Weekly</option>
-                    <option>Monthly</option>
-                    <option>Yearly</option>
-                  </Form.Control>
-
-                  <Form.Label>Ends On</Form.Label> */}
-      {/* <DateTimePicker
-                    onChange={setRecurringValue}
-                    value={recurringValue}
-                    disableClock={true}
-                  /> */}
-      {/* </Form.Group>
-              }
-            </Form>
-          </Modal.Body>
-
-          <Modal.Footer>
-            <Button onClick={() => setShow(false)} variant="outlined" color="secondary">Close</Button>
-            <Button onClick={saveEvent} variant="outlined" color="primary">Save changes</Button>
-          </Modal.Footer>
-        </Modal.Dialog>
-      </Modal> */}
-    </>
+    //       <Modal.Footer>
+    //         <Button onClick={() => setShow(false)} variant="outlined" color="secondary">Close</Button>
+    //         <Button onClick={saveEvent} variant="outlined" color="primary">Save changes</Button>
+    //       </Modal.Footer>
+    //     </Modal.Dialog>
+    //   </Modal> 
+    //</div>
   )
 }
 
